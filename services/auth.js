5dd523094185f4159d990023fefbf310843608eb
del42 <delger42@gmail.com>
2025-02-28 09:08:28 -0500

    Password encryption disabled

--- services/auth.js
+++ services/auth.js
@@ -3,9 +3,11 @@ const bcrypt = require("bcrypt");
 exports.getEncryptedPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, salt);
-  return hashPassword;
+  // return hashPassword;
+  return password;
 };
 
 exports.isValidPassword = (pwd, hash) => {
-  return bcrypt.compare(pwd, hash);
+  //return bcrypt.compare(pwd, hash);
+  return true
 };
