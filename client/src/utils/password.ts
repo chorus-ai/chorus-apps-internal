ab079fd12d758023e864a38912b24e3650c0efc0
del42 <delger42@gmail.com>
2026-08-19 20:20:05 -0400

    implement password validation utility function

--- client/src/utils/password.ts
+++ client/src/utils/password.ts
@@ -1,4 +1,4 @@
-export function validPassword(password) {
+export function validPassword(password: any) {
   return password.match(
     // /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@,_.?!#*])[a-zA-z\d$@,_.?!#*]{4,20}$/
     /^.{4,20}$/
